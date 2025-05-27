f14744016321606d71f0d41a970bae9b79769e72
Kurizu <83665497+crizmo@users.noreply.github.com>
2025-05-27 15:18:04 +0530

    KAnki and KWordle on KPM

---
+++ packages/kanki/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Install kanki"
+cp -r kanki /mnt/us/documents/kanki
+chmod +x /mnt/us/documents/kanki/kanki.sh
+echo "kanki package installed successfully."
---
+++ packages/kwordle/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Install kwordle"
+cp -r kwordle /mnt/us/documents/kwordle
+chmod +x /mnt/us/documents/kwordle/kwordle.sh
+echo "kwordle package installed successfully."
