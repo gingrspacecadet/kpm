94b35acb3264cd8da093e5d5ec567de64bfb6b85
Gingr <gingrspacecadet@github.com>
2025-06-14 15:27:46 +0000

    e

--- packages/apk/install.sh
+++ packages/apk/install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 mv /mnt/us/kpm/packages/apk/apk /usr/bin/apk
+mv /mnt/us/kpm/packages/apk/libcrypto.20.3 /usr/lib/libcrypto.20.3
+mv /mnt/us/kpm/packages/apk/libapk.so.2.14.0 /lib/libapk.so.2.14.0
