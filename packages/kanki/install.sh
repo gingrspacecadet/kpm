9c29d440ac3050fa8286e3f646850c5312a3230f
Gingr <gingrspacecadet@github.com>
2025-06-16 15:49:57 +0000

    Updated package kanki

--- packages/kanki/install.sh
+++ packages/kanki/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Installing kanki..."
-TEMP_DIR=/tmp/kpm
+TEMP_DIR=$(mktemp -d)
 echo "Downloading kanki package..."
 curl -L https://github.com/crizmo/KAnki/releases/download/v1.1.0/kanki.zip -o "$TEMP_DIR/kanki.zip"
 
@@ -9,7 +9,7 @@ unzip -q "$TEMP_DIR/kanki.zip" -d "$TEMP_DIR"
 
 echo "Installing to device..."
 mkdir -p /mnt/us/documents/kanki
-cp -r "$TEMP_DIR/kanki/kanki"/* /mnt/us/documents/
+cp -r "$TEMP_DIR/kanki/kanki"/* /mnt/us/documents/kanki/
 cp "$TEMP_DIR"/kanki.sh /mnt/us/documents/
 chmod +x /mnt/us/documents/kanki.sh
 
