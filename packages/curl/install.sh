f82f9eb1f06786abc7322e20e6de42b492d43ff6
Caitlyn <gingrspacecadet@arch.com>
2025-06-18 20:00:20 +0100

    Updated package curl

---
+++ packages/curl/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Installing curl..."
+
+cp /mnt/us/kpm/packages/curl /usr/bin/curl
+
+echo "curl package installed successfully."
---
+++ packages/curl/uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Uninstalling curl"
+
+rm /usr/bin/curl
+
+echo "curl package uninstalled successfully"
