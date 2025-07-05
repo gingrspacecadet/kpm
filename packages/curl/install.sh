a68da3dbbf4e74f06b3896c0197afafe2141b756
gingrspacecadet <jackrawlings2011@gmail.com>
2025-07-05 20:16:45 +0100

    Updated package curl

--- packages/curl/install.sh
+++ packages/curl/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Installing curl..."
 
-cp /mnt/us/kpm/packages/curl /usr/bin/curl
+cp /mnt/us/kpm/packages/curl/curl /usr/bin/curl
+cp /mnt/us/kpm/packages/curl/libcurl.so.4 /usr/lib/
 
 echo "curl package installed successfully."
